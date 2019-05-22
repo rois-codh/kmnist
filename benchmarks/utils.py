f45dc9151b45c0eb9e0df4df3727581f94e47bee
Lukas Biewald <lukas@wandb.com>
2019-05-22 20:01:49 +0000

    Change dataset back to kmnist

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -12,7 +12,7 @@ class KmnistCallback(Callback):
         # latest version of tensorflow
         wandb.log({"kmnist_val_acc" : logs["val_accuracy"]})
     elif "val_acc" in logs:
-        #older version of tensorflow
+        # older version of tensorflow
         wandb.log({"kmnist_val_acc" : logs["val_acc"]})
     else:
         raise Exception("Keras logs object missing validation accuracy")
@@ -22,11 +22,11 @@ def load(f):
     return np.load(f)['arr_0']
 
 def load_train_data(datadir):
-  x_train = load(os.path.join(datadir, 'k49-train-imgs.npz'))
-  y_train = load(os.path.join(datadir, 'k49-train-labels.npz'))
+  x_train = load(os.path.join(datadir, 'kmnist-train-imgs.npz'))
+  y_train = load(os.path.join(datadir, 'kmnist-train-labels.npz'))
   return x_train, y_train
 
 def load_test_data(datadir):
-  x_test = load(os.path.join(datadir, 'k49-test-imgs.npz'))
-  y_test = load(os.path.join(datadir, 'k49-test-labels.npz'))
+  x_test = load(os.path.join(datadir, 'kmnist-test-imgs.npz'))
+  y_test = load(os.path.join(datadir, 'kmnist-test-labels.npz'))
   return x_test, y_test
